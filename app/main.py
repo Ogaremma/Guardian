08256cdecc16943194261bc100b46f594c136235
Ogaremma <emmawills725@gmail.com>
2026-07-29 16:56:58 +0100

    setting up the server

--- app/main.py
+++ app/main.py
@@ -4,4 +4,24 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Welcome To Guardian"}
+    return {
+        "application": "Guardian",
+        "version": "1.0",
+        "status": "Running",
+        "message": "Guardian Security Monitoring System"
+    }
+
+@app.get("/health")
+def health():
+    return {
+        "status": "Healthy"
+    }
+
+@app.get("/about")
+def about():
+    return {
+        "engineer": "Ogar Emma",
+        "project": "Guardian",
+        "purpose": "Detect suspicious activities on websites"
+    }
+
