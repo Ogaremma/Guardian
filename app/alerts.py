87d9b6cb0e2953d65952c4602eff762d70a98a1c
Ogaremma <emmawills725@gmail.com>
2026-08-02 18:31:40 +0100

    set up error handling and email algo

--- app/alerts.py
+++ app/alerts.py
@@ -25,3 +25,43 @@ def send_alert(
     message["From"] = sender_email
     message["To"] = recipient_email
     message["Subject"] = "🚨 GUARDIAN SECURITY ALERTS"
+
+    body = f"""
+    🚨 GUARDIAN SECURITY ALERT
+
+    Website: {website}
+
+    Attack Type: {attack_type}
+
+    Severity: {severity}
+
+    IP Address: {source_ip}
+
+    Username Used: {username}
+
+    Recommended Action:
+    Review logs immediately.
+    """
+
+    message.attach(MIMEText(body, "plain"))
+
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+
+        server.starttls()
+
+        server.login(sender_email, sender_password)
+
+        server.sendmail(
+            sender_email,
+            recipient_email,
+            message.as_string()
+        )
+
+        server.quit()
+
+        print("Email alert sent successfully.")
+
+    except Exception as error:
+        print("Failed to send email.")
+        print(error)
