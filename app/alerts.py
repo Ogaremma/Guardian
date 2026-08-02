4a3d5d6a9ebb88c37154287459edb38f2dc64485
Ogaremma <emmawills725@gmail.com>
2026-08-02 17:35:12 +0100

    Stop tracking venv and env files; add .gitignore

---
+++ app/alerts.py
@@ -0,0 +1,27 @@
+import smtplib
+import os
+
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+from dotenv import load_dotenv
+
+
+def send_alert(
+    recipient_email,
+    website,
+    attack_type,
+    severity,
+    source_ip,
+    username
+):
+    load_dotenv()
+
+    sender_email = os.getenv("GUARDIAN_EMAIL")
+    sender_password = os.getenv("GUARDIAN_PASSWORD")
+
+    message = MIMEMultipart()
+
+    message["From"] = sender_email
+    message["To"] = recipient_email
+    message["Subject"] = "🚨 GUARDIAN SECURITY ALERT"
